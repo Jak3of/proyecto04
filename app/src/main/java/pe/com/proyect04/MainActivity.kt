2bb8f5802bcfb060a68387f1948265bfbffb8117
SVHugo <hugosvalencia7@gmail.com>
2022-08-01 22:37:05 -0500

    otro comentario

--- app/src/main/java/pe/com/proyect04/MainActivity.kt
+++ app/src/main/java/pe/com/proyect04/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    // Hugo estuvo aqui
+        // Hugo estuvo aqui
+        // otro comentario
+
     }
 }
